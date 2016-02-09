b316ebaabc62716372bc9098d185ebff4538cc35
Ryan Worcester <rworcest@g.emporia.edu>
2016-02-09 16:43:17 -0600

    Added DicePool.roll without validation etc

---
+++ tests/DicePool.js
@@ -0,0 +1,25 @@
+'use strict';
+Tester.DicePool = {};
+Tester.DicePool.roll = function(isFirst)
+{
+   TesterUtility.clearResults(isFirst);
+
+   var testResults = [], actual, generator;
+
+   try{
+   generator = new NonRandomNumberGenerator([(4/8), (7/8), (11/16), (15/16)]).generate;
+   actual = new DicePool('2d8+2d16', [
+      {
+         die: new Die(8),
+         count: 2
+      },
+      {
+         die: new Die(16),
+         count: 2
+      }
+   ]).roll(generator);
+   testResults.push({Expected: [5, 8, 12, 16], Actual: actual, Description: 'Happy path 2d8+2d16'});
+   } catch(e){testResults.push({Error: e, Action: 'Happy path 2d8+2d16'});}
+
+   TesterUtility.displayResults('DicePool new DicePool().roll()', testResults, isFirst);
+};
--- tests/include.js
+++ tests/include.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var fileNames = ['Die', 'prototypes'];
+var fileNames = ['DicePool', 'Die', 'prototypes'];
 if(undefined === this['includePath']) this.includePath = '';
 
 for (var i = 0; i < fileNames.length; ++i)
