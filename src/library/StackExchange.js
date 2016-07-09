62a6a39f6bda03606e1400b0f32e7b343fbad847
Ryan Worcester <rworcest@g.emporia.edu>
2016-07-09 15:52:05 -0500

    Made failing tests for StackExchange

--- src/library/StackExchange.js
+++ src/library/StackExchange.js
@@ -1,5 +1,12 @@
 'use strict';
-//Despite the tests this whole file should be considered beta until I can incorporate it into Statistics
+/*Despite the tests this whole file should be considered beta until I can incorporate it into Statistics:
+1. Finish (die with no: constantModifier, rerollCriteria, explodeType)
+2. Make as generic as possible (likely multiple steps)
+   a. die with: constantModifier, rerollCriteria, explodeType
+   b. any number of drop
+   c. any kind of drop, negative
+3. Optimize
+4. Put into Statistics*/
 var beta = {StackExchange: {}};
 
 /**from: http://stats.stackexchange.com/questions/130025/formula-for-dropping-dice-non-brute-force
@@ -14,7 +21,7 @@ beta.StackExchange.probabilityThatSumOfDiceIsA = function(a, die, dieCount)
    also assuming that dieCount >= 1*/
 
    var sideCount = die.toJSON().sideCount;
-   if(a < dieCount || a > (dieCount*sideCount)) return 0;
+   if(0 === dieCount || a < dieCount || a > (dieCount*sideCount)) return 0;
    if(1 === dieCount) return (1 / sideCount);
 
    var result = 0;
@@ -26,36 +33,39 @@ beta.StackExchange.probabilityThatSumOfDiceIsA = function(a, die, dieCount)
    return result;
 }
 
-/**Now suppose Z(n) is the sum of n dice when one die is dropped. Then
-~
+/**Now suppose Z(n) is the sum of n dice when [the smallest] die is dropped. Then
+
+p(nth die is the smallest)p(Y(n-1)=a)+p(nth die is not the smallest)sumOverAllK(p(Z(n-1)=a-k)p(X(n)=k))
+
 If we define M(n) to be distribution of the minimum of n dies, then*/
 function probabilityThat_ZofNIsA(a, die, dieCount)
 {
    var sideCount = die.toJSON().sideCount;
    var diceKept = (dieCount-1);
    if(0 === diceKept || a < diceKept || a > (diceKept*sideCount)) return 0;
-   if(1 === diceKept) return /*?*/;
 
-   var result = (probabilityThat_XofNisSmallest(/*?*/) * beta.StackExchange.probabilityThatSumOfDiceIsA(a, die, (dieCount-1)));
+   var result = (probabilityThat_XofNisSmallest(die, dieCount) * beta.StackExchange.probabilityThatSumOfDiceIsA(a, die, (dieCount-1)));
 
-   var probabilityThat_XofNisNotSmallest = probabilityThat_XofNisNotSmallest(/*?*/);
+   var probabilityThat_XofNisNotSmallestResult = probabilityThat_XofNisNotSmallest(die, dieCount);
    var secondPart = 0;
    for (var k = 1; k <= sideCount; ++k)
    {
       secondPart += (probabilityThat_ZofNIsA((a-k), die, (dieCount-1)) *
-      probabilityThat_XofNIsA_GivenThatXofNisNotSmallest(/*?*/));
+      probabilityThat_XofNIsA_GivenThatXofNisNotSmallest(a, die, dieCount));
    }
-   secondPart *= probabilityThat_XofNisNotSmallest;
+   secondPart *= probabilityThat_XofNisNotSmallestResult;
 
    result += secondPart;
    return result;
 }
 
 /**and we can calculate M(n) using*/
-function probabilityThat_theSmallestIsA(/*?*/)
+function probabilityThat_theSmallestIsA(a, die, dieCount)
 {
-   var result = probabilityThat_XofNisSmallest(/*?*/) * probabilityThat_XofNIsA_GivenThatXofNisTheSmallest(/*?*/);
-   result += (probabilityThat_XofNisNotSmallest(/*?*/) * probabilityThat_theSmallestIsA_GivenThatXofNisNotSmallest(/*?*/));
+   if(1 === dieCount) return (1 / die.toJSON().sideCount);  //this is probabilityThat_XofNIsA
+
+   var result = probabilityThat_XofNisSmallest(die, dieCount) * probabilityThat_XofNIsA_GivenThatXofNisTheSmallest(a, die, dieCount);
+   result += (probabilityThat_XofNisNotSmallest(die, dieCount) * probabilityThat_theSmallestIsA_GivenThatXofNisNotSmallest(a, die, dieCount));
    return result;
 }
 
@@ -64,15 +74,18 @@ function probabilityThat_theSmallestIsA(/*?*/)
 edit: A comment has been raised on how to calculate p(X(n)<=M(n-1)). Since X(n),M(n-1) can each only take on one of six values, we can just sum over all possibilities:*/
 function probabilityThat_XofNisSmallest(die, dieCount)
 {
+   if(1 === dieCount) return 1;
+
    var sideCount = die.toJSON().sideCount;
    var result = 0;
    for (var a = 1; a <= sideCount; ++a)
    {
       for (var b = 1; b <= sideCount; ++b)
       {
-         if(a <= b) result += ((1 / sideCount) * probabilityThat_theSmallestIsA(/*? with dieCount-1*/));
+         if(a <= b) result += ((1 / sideCount) * probabilityThat_theSmallestIsA(b, die, (dieCount-1)));
       }
    }
+   return result;
 }
 
 /*Similarly, p(X(n)=k|X(n)>M(n-1)) can be calculated by applying Bayes rule then summing over the possible values of X(n),M(n-1).*/
@@ -82,9 +95,22 @@ function probabilityThat_XofNisNotSmallest(die, dieCount)
 {
    return (1-probabilityThat_XofNisSmallest(die, dieCount));
 }
-function probabilityThat_XofNIsA_GivenThatXofNisNotSmallest(){}  //TODO: stub
-function probabilityThat_XofNIsA_GivenThatXofNisTheSmallest(){}  //TODO: stub
-function probabilityThat_theSmallestIsA_GivenThatXofNisNotSmallest(){}  //TODO: stub
+function probabilityThat_XofNIsA_GivenThatXofNisNotSmallest(a, die, dieCount)
+{
+   //TODO: wrong?:
+   return (1 / die.toJSON().sideCount);  //this is probabilityThat_XofNIsA
+}
+
+function probabilityThat_XofNIsA_GivenThatXofNisTheSmallest(a, die, dieCount)
+{
+   //TODO: wrong?:
+   return (1 / die.toJSON().sideCount);  //this is probabilityThat_XofNIsA
+}
+function probabilityThat_theSmallestIsA_GivenThatXofNisNotSmallest(a, die, dieCount)
+{
+   //TODO: wrong?:
+   return probabilityThat_theSmallestIsA(a, die, dieCount);
+}
 
 /**@returns the probability of of A given B.*/
 function bayesTheorem(probabilityOfBGivenA, probabilityOfA, probabilityOfB)
--- tests/StackExchange.js
+++ tests/StackExchange.js
@@ -31,3 +31,79 @@ TestSuite.StackExchange.probabilityThatSumOfDiceIsA = function(isFirst)
 
    return TestRunner.displayResults('StackExchange beta.StackExchange.probabilityThatSumOfDiceIsA', testResults, isFirst);
 };
+TestSuite.StackExchange.probabilityThat_ZofNIsA = function(isFirst)
+{
+   TestRunner.clearResults(isFirst);
+
+   var testResults = [], actual, expected;
+
+   try{
+   actual = probabilityThat_ZofNIsA(1, new Die(2), 2);
+   //actual is currently: 3/8
+   testResults.push({Expected: (1/4), Actual: actual, Description: 'chance of 1 from 2d2 drop lowest'});
+   } catch(e){testResults.push({Error: e, Description: 'chance of 1 from 2d2 drop lowest'});}
+
+   try{
+   actual = probabilityThat_ZofNIsA(4, new Die(4), 4);
+   expected = Statistics.calculateDiceSums(new DicePool('4d4dl'));
+   Statistics.determineProbability(expected);
+   console.assert(4 === expected[4-1-2].result);  //probability 0 is excluded so results 1-2 aren't here.
+   //currently fails
+   testResults.push({Expected: expected[1].probability, Actual: actual, Description: 'chance of 4 from 445 drop lowest'});
+   } catch(e){testResults.push({Error: e, Description: 'chance of 4 from 4d4 drop lowest'});}
+
+   return TestRunner.displayResults('StackExchange probabilityThat_ZofNIsA', testResults, isFirst);
+};
+TestSuite.StackExchange.probabilityThat_theSmallestIsA = function(isFirst)
+{
+   TestRunner.clearResults(isFirst);
+
+   var testResults = [], actual, expected;
+
+   try{
+   actual = probabilityThat_theSmallestIsA(1, new Die(2), 2);
+   //11, 12, 21, 22
+   testResults.push({Expected: (3/4), Actual: actual, Description: '1 using 2d2'});
+   } catch(e){testResults.push({Error: e, Description: '1 using 2d2'});}
+
+   try{
+   actual = probabilityThat_theSmallestIsA(2, new Die(2), 2);
+   //11, 12, 21, 22
+   testResults.push({Expected: (1/4), Actual: actual, Description: '2 using 2d2'});
+   } catch(e){testResults.push({Error: e, Description: '2 using 2d2'});}
+
+   return TestRunner.displayResults('StackExchange probabilityThat_theSmallestIsA', testResults, isFirst);
+};
+TestSuite.StackExchange.probabilityThat_XofNisSmallest = function(isFirst)
+{
+   TestRunner.clearResults(isFirst);
+
+   var testResults = [], actual, expected;
+
+   try{
+   actual = probabilityThat_XofNisSmallest(new Die(2), 2);
+   //11, 12, 21, 22
+   //maybe something like:
+   //(I roll 1)(you roll 1) + (I roll 1)(you roll 2) + (I roll 2)(you roll 2)
+   //(1/2)*(1/2) * 3 = 3/4
+   testResults.push({Expected: (3/4), Actual: actual, Description: '2d2'});
+   } catch(e){testResults.push({Error: e, Description: '2d2'});}
+
+   try{
+   actual = probabilityThat_XofNisSmallest(new Die(3), 2);
+   //all: 11, 12, 13, 21, 22, 23, 31, 32, 33
+   //(I roll 1)(you roll any) + (me 2)(you 2 or 3) + (me 3)(you 3)
+   //(1/3) + (1/3)(2/3) + (1/3)(1/3) = 3/9 + 2/9 + 1/9 = 6/9
+   testResults.push({Expected: (2/3), Actual: actual, Description: '2d3'});
+   } catch(e){testResults.push({Error: e, Description: '2d3'});}
+
+   try{
+   actual = probabilityThat_XofNisSmallest(new Die(2), 3);
+   //all: 111, 112, 121, 122, 211, 212, 221, 222
+   //(me 1) + (me 2)(not 1)(not 1)
+   //(1/2) + (1/2)^3 = 4/8 + 1/8
+   testResults.push({Expected: (5/8), Actual: actual, Description: '3d2'});
+   } catch(e){testResults.push({Error: e, Description: '3d2'});}
+
+   return TestRunner.displayResults('StackExchange probabilityThat_XofNisSmallest', testResults, isFirst);
+};
