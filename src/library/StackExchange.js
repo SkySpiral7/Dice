7758ae33ddd52e5e6b65d6fcb1e8589afcf8e1fe
Ryan Worcester <rworcest@g.emporia.edu>
2016-07-09 11:39:05 -0500

    StackExchange: stubs

--- src/library/StackExchange.js
+++ src/library/StackExchange.js
@@ -2,7 +2,7 @@
 //Despite the tests this whole file should be considered beta until I can incorporate it into Statistics
 var beta = {StackExchange: {}};
 
-/*from: http://stats.stackexchange.com/questions/130025/formula-for-dropping-dice-non-brute-force
+/**from: http://stats.stackexchange.com/questions/130025/formula-for-dropping-dice-non-brute-force
 I'm not sure about the general problem with varying numbers of dice, sides, and drops, but I think I can see an efficient algorithm for the drop-1 case. The qualifier is that I'm not completely sure that it's correct, but right now I can't see any flaws.
 
 Let's start by not dropping any dice. Suppose X(n) represents the nth die, and suppose Y(n) represents the sum of n dice. Then*/
@@ -26,34 +26,68 @@ beta.StackExchange.probabilityThatSumOfDiceIsA = function(a, die, dieCount)
    return result;
 }
 
-/*
-function probabilityThat_ZofNIsA(a, diceArray){
-   //assuming: homogeneous dice pool (such as 4d10), exactly 1 die is dropped (and it is the lowest), no rerolls or min/max, standard die only
-   //also assuming that a is a possible sum for the pool
-   //var diceArray = poolGiven.getAllDice();
-   var n = diceArray.length;
-   var result = (probabilityThat_XofNisSmallest(?) * probabilityThat_YofNisA(a, diceArray.slice(1)));
-   result += (probabilityThat_XofNisNotSmallest(?) * bigSum(a, diceArray.slice(1)));
+/**Now suppose Z(n) is the sum of n dice when one die is dropped. Then
+~
+If we define M(n) to be distribution of the minimum of n dies, then*/
+function probabilityThat_ZofNIsA(a, die, dieCount)
+{
+   var sideCount = die.toJSON().sideCount;
+   var diceKept = (dieCount-1);
+   if(0 === diceKept || a < diceKept || a > (diceKept*sideCount)) return 0;
+   if(1 === diceKept) return /*?*/;
+
+   var result = (probabilityThat_XofNisSmallest(/*?*/) * beta.StackExchange.probabilityThatSumOfDiceIsA(a, die, (dieCount-1)));
+
+   var probabilityThat_XofNisNotSmallest = probabilityThat_XofNisNotSmallest(/*?*/);
+   var secondPart = 0;
+   for (var k = 1; k <= sideCount; ++k)
+   {
+      secondPart += (probabilityThat_ZofNIsA((a-k), die, (dieCount-1)) *
+      probabilityThat_XofNIsA_GivenThatXofNisNotSmallest(/*?*/));
+   }
+   secondPart *= probabilityThat_XofNisNotSmallest;
+
+   result += secondPart;
+   return result;
 }
 
-function bigSum(a, diceArray){
-   var n = diceArray.length;
+/**and we can calculate M(n) using*/
+function probabilityThat_theSmallestIsA(/*?*/)
+{
+   var result = probabilityThat_XofNisSmallest(/*?*/) * probabilityThat_XofNIsA_GivenThatXofNisTheSmallest(/*?*/);
+   result += (probabilityThat_XofNisNotSmallest(/*?*/) * probabilityThat_theSmallestIsA_GivenThatXofNisNotSmallest(/*?*/));
+   return result;
 }
 
-function probabilityThat_XofNisSmallest(?){
+/**Anyway, together this all suggests a dynamic programming algorithm based on Y(n), Z(n), and M(n). Should be quadratic in n.
+
+edit: A comment has been raised on how to calculate p(X(n)<=M(n-1)). Since X(n),M(n-1) can each only take on one of six values, we can just sum over all possibilities:*/
+function probabilityThat_XofNisSmallest(die, dieCount)
+{
+   var sideCount = die.toJSON().sideCount;
+   var result = 0;
+   for (var a = 1; a <= sideCount; ++a)
+   {
+      for (var b = 1; b <= sideCount; ++b)
+      {
+         if(a <= b) result += ((1 / sideCount) * probabilityThat_theSmallestIsA(/*? with dieCount-1*/));
+      }
+   }
 }
 
-function probabilityThat_XofNisNotSmallest(?){
-}
+/*Similarly, p(X(n)=k|X(n)>M(n-1)) can be calculated by applying Bayes rule then summing over the possible values of X(n),M(n-1).*/
 
-function probabilityThat_YofNisA(a, diceArray){
-   var n = diceArray.length;
-   var finishedPolys=[];
-   for(var i=0; i < diceArray.length; i++){finishedPolys.push(Polynomial.createDiePolynomial(diceArray[i], 0));}  //0 indicates no explosions yet
-   var rollData=Polynomial.multiplyPolynomials(finishedPolys);
-
-   var totalFreq=0;
-   for(var i=0; i < rollData.length; i++){totalFreq+=rollData[i][1];}
-   for(var i=0; i < rollData.length; i++){if(rollData[i][0] === a) return (rollData[i][1] / totalFreq);}
+//others:
+function probabilityThat_XofNisNotSmallest(die, dieCount)
+{
+   return (1-probabilityThat_XofNisSmallest(die, dieCount));
 }
-/**/
+function probabilityThat_XofNIsA_GivenThatXofNisNotSmallest(){}  //TODO: stub
+function probabilityThat_XofNIsA_GivenThatXofNisTheSmallest(){}  //TODO: stub
+function probabilityThat_theSmallestIsA_GivenThatXofNisNotSmallest(){}  //TODO: stub
+
+/**@returns the probability of of A given B.*/
+function bayesTheorem(probabilityOfBGivenA, probabilityOfA, probabilityOfB)
+{
+   return (probabilityOfBGivenA * probabilityOfA / probabilityOfB);
+}
