f6c7de7332db4f4e82f3eadc25be7d7e9a79e865
Ryan Worcester <rworcest@g.emporia.edu>
2016-07-16 16:44:25 -0500

    Changed StackExchange namespace to beta

--- src/library/StackExchange.js
+++ src/library/StackExchange.js
@@ -1,5 +1,5 @@
 'use strict';
-/*Despite the tests this whole file should be considered beta until I can incorporate it into Statistics:
+/*Despite tests existing, this whole file should be considered beta until I can incorporate it into Statistics:
 1. Finish (die with no: constantModifier, rerollCriteria, explodeType)
 2. Optimize
 3. Compare to brute force
@@ -11,13 +11,13 @@
    c. any kind of drop, negative
 5. Compare again to make sure this can still handle 10k1 etc
 6. Put into Statistics*/
-var beta = {StackExchange: {}};
+var beta = {};
 
 /**from: http://stats.stackexchange.com/questions/130025/formula-for-dropping-dice-non-brute-force
 I'm not sure about the general problem with varying numbers of dice, sides, and drops, but I think I can see an efficient algorithm for the drop-1 case. The qualifier is that I'm not completely sure that it's correct, but right now I can't see any flaws.
 
 Let's start by not dropping any dice. Suppose X(n) represents the nth die, and suppose Y(n) represents the sum of n dice. Then*/
-beta.StackExchange.probabilityThatSumOfDiceIsA = function(a, die, dieCount)
+beta.probabilityThatSumOfDiceIsA = function(a, die, dieCount)
 {
    /*This function shows that I understand how to read the statistics equations (I wasn't sure if I could)
    and that the basis of his formulas works.
@@ -31,7 +31,7 @@ beta.StackExchange.probabilityThatSumOfDiceIsA = function(a, die, dieCount)
    var result = 0;
    for (var k = 1; k <= sideCount; ++k)
    {
-      result += (beta.StackExchange.probabilityThatSumOfDiceIsA((a-k), die, (dieCount-1)) *
+      result += (beta.probabilityThatSumOfDiceIsA((a-k), die, (dieCount-1)) *
       (1 / sideCount));
    }
    return result;
@@ -42,7 +42,7 @@ beta.StackExchange.probabilityThatSumOfDiceIsA = function(a, die, dieCount)
 p(nth die is the smallest)p(Y(n-1)=a|nth die is the smallest) + p(nth die is not the smallest)sumOverAllK(p(Z(n-1)=(a-k))p(X(n)=k))
 
 If we define M(n) to be distribution of the minimum of n dies, then*/
-function probabilityThat_ZofNIsA(a, die, dieCount)
+beta.probabilityThat_ZofNIsA = function(a, die, dieCount)
 {
    var sideCount = die.toJSON().sideCount;
    var diceKept = (dieCount-1);
@@ -50,21 +50,21 @@ function probabilityThat_ZofNIsA(a, die, dieCount)
    if(0 === diceKept || a < diceKept || a > (diceKept*sideCount)) return 0;
    var diceCountExcludingSelf = (dieCount-1);  //same value as diceKept but different meaning
 
-   var result = (beta.StackExchange.probabilityThat_XofNisSmallestOrEqual(die, dieCount) *
-      probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(a, die, diceKept));
+   var result = (beta.probabilityThat_XofNisSmallestOrEqual(die, dieCount) *
+      beta.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(a, die, diceKept));
 
-   var probabilityThat_XofNisLargestResult = beta.StackExchange.probabilityThat_XofNisLargest(die, dieCount);
+   var probabilityThat_XofNisLargestResult = beta.probabilityThat_XofNisLargest(die, dieCount);
    var secondPart = 0;
    for (var k = 1; k <= sideCount; ++k)
    {
-      secondPart += (probabilityThat_ZofNIsA((a-k), die, diceCountExcludingSelf) *
-      probabilityThat_XofNIsA_GivenThatXofNisLargest(k, die, dieCount));
+      secondPart += (beta.probabilityThat_ZofNIsA((a-k), die, diceCountExcludingSelf) *
+      beta.probabilityThat_XofNIsA_GivenThatXofNisLargest(k, die, dieCount));
    }
    secondPart *= probabilityThat_XofNisLargestResult;
 
    result += secondPart;
    return result;
-}
+};
 /**This is only for testing and will be deleted later. It helps test Z.
 It returns the brute force answer which is what probabilityThat_ZofNIsA is trying to find.
 This function's answer is known to be correct and thus can be used to compare or rule out.*/
@@ -105,7 +105,7 @@ This is the correct answer
 Anyway, together this all suggests a dynamic programming algorithm based on Y(n), Z(n), and M(n). Should be quadratic in n.
 
 edit: A comment has been raised on how to calculate p(X(n)<=M(n-1)). Since X(n),M(n-1) can each only take on one of six values, we can just sum over all possibilities:*/
-beta.StackExchange.probabilityThat_XofNisSmallestOrEqual = function(die, dieCount)
+beta.probabilityThat_XofNisSmallestOrEqual = function(die, dieCount)
 {
    //this implementation is based on how I calculated it by hand rather than based on his formula
    var sideCount = die.toJSON().sideCount;
@@ -114,7 +114,7 @@ beta.StackExchange.probabilityThat_XofNisSmallestOrEqual = function(die, dieCoun
    var result = 0;
    for (var myRoll = 1; myRoll <= sideCount; ++myRoll)
    {
-      result += probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(myRoll, die, dieCount) *
+      result += beta.probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(myRoll, die, dieCount) *
          (1/sideCount);  //it's the prob for rolling myRoll
    }
 
@@ -124,22 +124,22 @@ beta.StackExchange.probabilityThat_XofNisSmallestOrEqual = function(die, dieCoun
 /*Similarly, p(X(n)=k|X(n)>M(n-1)) can be calculated by applying Bayes rule then summing over the possible values of X(n),M(n-1).*/
 
 //others:
-beta.StackExchange.probabilityThat_XofNisLargest = function(die, dieCount)
+beta.probabilityThat_XofNisLargest = function(die, dieCount)
 {
-   return (1-beta.StackExchange.probabilityThat_XofNisSmallestOrEqual(die, dieCount));
+   return (1-beta.probabilityThat_XofNisSmallestOrEqual(die, dieCount));
 };
-function probabilityThat_XofNIsA_GivenThatXofNisLargest(a, die, dieCount)
+beta.probabilityThat_XofNIsA_GivenThatXofNisLargest = function(a, die, dieCount)
 {
    var probabilityThat_XofNIsA = (1 / die.toJSON().sideCount);
    if(1 === dieCount) return probabilityThat_XofNIsA;  //with only 1 die being the largest means nothing
-   var probabilityThat_XofNisLargest = beta.StackExchange.probabilityThat_XofNisLargest(die, dieCount);
+   var probabilityThat_XofNisLargest = beta.probabilityThat_XofNisLargest(die, dieCount);
 
-   var probabilityThat_XofNisLargest_GivenThatXofNIsAResult = probabilityThat_XofNisLargest_GivenThatXofNIsA(a, die, dieCount);
+   var probabilityThat_XofNisLargest_GivenThatXofNIsAResult = beta.probabilityThat_XofNisLargest_GivenThatXofNIsA(a, die, dieCount);
 
    return bayesTheorem(probabilityThat_XofNisLargest_GivenThatXofNIsAResult, probabilityThat_XofNIsA, probabilityThat_XofNisLargest);
-}
+};
 
-function probabilityThat_XofNisLargest_GivenThatXofNIsA(a, die, dieCount)
+beta.probabilityThat_XofNisLargest_GivenThatXofNIsA = function(a, die, dieCount)
 {
    if(1 === dieCount) return 1;  //fast path
    if(1 === a) return 0;  //since this is the smallest possible number it can't be strictly greater
@@ -149,8 +149,8 @@ function probabilityThat_XofNisLargest_GivenThatXofNIsA(a, die, dieCount)
    var probAllOthersRolledLower = Math.pow(probOtherRolledLower, (dieCount-1));
 
    return probAllOthersRolledLower;
-}
-function probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(a, die, dieCount)
+};
+beta.probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA = function(a, die, dieCount)
 {
    if(1 === dieCount) return 1;  //fast path
    var sideCount = die.toJSON().sideCount;
@@ -160,8 +160,8 @@ function probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(a, die, dieCount
    var probAllOthersRolledEqualOrHigher = Math.pow(probOtherRolledEqualOrHigher, (dieCount-1));
 
    return probAllOthersRolledEqualOrHigher;
-}
-function probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(a, die, dieKept)
+};
+beta.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual = function(a, die, dieKept)
 {
    //base cases are covered by probabilityThat_ZofNIsA
    var sideCount = die.toJSON().sideCount;
@@ -186,7 +186,7 @@ function probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(a, die, dieK
       if(a === diceResults[findIndex].result) return diceResults[findIndex].probability;
    }
    return 0;
-}
+};
 
 /**@returns the probability of of A given B.*/
 function bayesTheorem(probabilityOfBGivenA, probabilityOfA, probabilityOfB)
--- tests/StackExchange.js
+++ tests/StackExchange.js
@@ -7,12 +7,12 @@ TestSuite.StackExchange.probabilityThatSumOfDiceIsA = function(isFirst)
    var testResults = [], actual, expected;
 
    try{
-   actual = beta.StackExchange.probabilityThatSumOfDiceIsA(2, new Die(2), 2);
+   actual = beta.probabilityThatSumOfDiceIsA(2, new Die(2), 2);
    testResults.push({Expected: (1/4), Actual: actual, Description: '2d2 rolled 2'});
    } catch(e){testResults.push({Error: e, Description: '2d2 rolled 2'});}
 
    try{
-   actual = beta.StackExchange.probabilityThatSumOfDiceIsA(20, new Die(10), 10);
+   actual = beta.probabilityThatSumOfDiceIsA(20, new Die(10), 10);
    expected = Statistics.calculateDiceSums(new DicePool('10d10'));
    Statistics.determineProbability(expected);
    console.assert(20 === expected[20-1-9].result);  //probability 0 is excluded so results 1-9 aren't here.
@@ -20,16 +20,16 @@ TestSuite.StackExchange.probabilityThatSumOfDiceIsA = function(isFirst)
    } catch(e){testResults.push({Error: e, Description: '10d10 rolled 20'});}
 
    try{
-   actual = beta.StackExchange.probabilityThatSumOfDiceIsA(1, new Die(2), 2);
+   actual = beta.probabilityThatSumOfDiceIsA(1, new Die(2), 2);
    testResults.push({Expected: 0, Actual: actual, Description: 'A too low'});
    } catch(e){testResults.push({Error: e, Description: 'A too low'});}
 
    try{
-   actual = beta.StackExchange.probabilityThatSumOfDiceIsA(5, new Die(2), 2);
+   actual = beta.probabilityThatSumOfDiceIsA(5, new Die(2), 2);
    testResults.push({Expected: 0, Actual: actual, Description: 'A too high'});
    } catch(e){testResults.push({Error: e, Description: 'A too high'});}
 
-   return TestRunner.displayResults('StackExchange beta.StackExchange.probabilityThatSumOfDiceIsA', testResults, isFirst);
+   return TestRunner.displayResults('StackExchange beta.probabilityThatSumOfDiceIsA', testResults, isFirst);
 };
 TestSuite.StackExchange.probabilityThat_ZofNIsA = function(isFirst)
 {
@@ -38,13 +38,13 @@ TestSuite.StackExchange.probabilityThat_ZofNIsA = function(isFirst)
    var testResults = [], actual, expected;
 
    try{
-   actual = probabilityThat_ZofNIsA(1, new Die(2), 2);
+   actual = beta.probabilityThat_ZofNIsA(1, new Die(2), 2);
    //all: 11, 12, 21, 22 which become: 1, 2, 2, 2
    testResults.push({Expected: (1/4), Actual: actual, Description: 'chance of 1 from 2d2 drop lowest'});
    } catch(e){testResults.push({Error: e, Description: 'chance of 1 from 2d2 drop lowest'});}
 
    try{
-   actual = probabilityThat_ZofNIsA(4, new Die(4), 4);
+   actual = beta.probabilityThat_ZofNIsA(4, new Die(4), 4);
    expected = Statistics.calculateDiceSums(new DicePool('4d4 drop lowest'));
    Statistics.determineProbability(expected);
    console.assert(4 === expected[4-1-2].result);  //probability 0 is excluded so results 1 and 2 aren't here.
@@ -52,7 +52,7 @@ TestSuite.StackExchange.probabilityThat_ZofNIsA = function(isFirst)
    testResults.push({Expected: expected[1].probability, Actual: actual, Description: 'chance of 4 from 4d4 drop lowest'});
    } catch(e){testResults.push({Error: e, Description: 'chance of 4 from 4d4 drop lowest'});}
 
-   return TestRunner.displayResults('StackExchange probabilityThat_ZofNIsA', testResults, isFirst);
+   return TestRunner.displayResults('StackExchange beta.probabilityThat_ZofNIsA', testResults, isFirst);
 };
 TestSuite.StackExchange.probabilityThat_XofNisSmallestOrEqual = function(isFirst)
 {
@@ -61,12 +61,12 @@ TestSuite.StackExchange.probabilityThat_XofNisSmallestOrEqual = function(isFirst
    var testResults = [], actual;
 
    try{
-   actual = beta.StackExchange.probabilityThat_XofNisSmallestOrEqual(new Die(2), 1);
+   actual = beta.probabilityThat_XofNisSmallestOrEqual(new Die(2), 1);
    testResults.push({Expected: 1, Actual: actual, Description: '1d2'});
    } catch(e){testResults.push({Error: e, Description: '1d2'});}
 
    try{
-   actual = beta.StackExchange.probabilityThat_XofNisSmallestOrEqual(new Die(2), 2);
+   actual = beta.probabilityThat_XofNisSmallestOrEqual(new Die(2), 2);
    //all: 11, 12, 21, 22
    //maybe something like:
    //(I roll 1)(you roll 1) + (I roll 1)(you roll 2) + (I roll 2)(you roll 2)
@@ -75,7 +75,7 @@ TestSuite.StackExchange.probabilityThat_XofNisSmallestOrEqual = function(isFirst
    } catch(e){testResults.push({Error: e, Description: '2d2'});}
 
    try{
-   actual = beta.StackExchange.probabilityThat_XofNisSmallestOrEqual(new Die(3), 2);
+   actual = beta.probabilityThat_XofNisSmallestOrEqual(new Die(3), 2);
    //all: 11, 12, 13, 21, 22, 23, 31, 32, 33
    //(I roll 1)(you roll any) + (me 2)(you 2 or 3) + (me 3)(you 3)
    //(1/3) + (1/3)(2/3) + (1/3)(1/3) = 3/9 + 2/9 + 1/9 = 6/9
@@ -83,14 +83,14 @@ TestSuite.StackExchange.probabilityThat_XofNisSmallestOrEqual = function(isFirst
    } catch(e){testResults.push({Error: e, Description: '2d3'});}
 
    try{
-   actual = beta.StackExchange.probabilityThat_XofNisSmallestOrEqual(new Die(2), 3);
+   actual = beta.probabilityThat_XofNisSmallestOrEqual(new Die(2), 3);
    //all: 111, 112, 121, 122, 211, 212, 221, 222
    //(me 1) + (me 2)(not 1)(not 1)
    //(1/2) + (1/2)^3 = 4/8 + 1/8
    testResults.push({Expected: (5/8), Actual: actual, Description: '3d2'});
    } catch(e){testResults.push({Error: e, Description: '3d2'});}
 
-   return TestRunner.displayResults('StackExchange beta.StackExchange.probabilityThat_XofNisSmallestOrEqual', testResults, isFirst);
+   return TestRunner.displayResults('StackExchange beta.probabilityThat_XofNisSmallestOrEqual', testResults, isFirst);
 };
 TestSuite.StackExchange.probabilityThat_XofNIsA_GivenThatXofNisLargest = function(isFirst)
 {
@@ -99,23 +99,23 @@ TestSuite.StackExchange.probabilityThat_XofNIsA_GivenThatXofNisLargest = functio
    var testResults = [], actual;
 
    try{
-   actual = probabilityThat_XofNIsA_GivenThatXofNisLargest(1, new Die(2), 1);
+   actual = beta.probabilityThat_XofNIsA_GivenThatXofNisLargest(1, new Die(2), 1);
    testResults.push({Expected: (1/2), Actual: actual, Description: '1 from 1d2'});
    } catch(e){testResults.push({Error: e, Description: '1 from 1d2'});}
 
    try{
-   actual = probabilityThat_XofNIsA_GivenThatXofNisLargest(1, new Die(2), 2);
+   actual = beta.probabilityThat_XofNIsA_GivenThatXofNisLargest(1, new Die(2), 2);
    //all: 11, 12, 21, 22 so x is: -, 2, 2, -
    testResults.push({Expected: 0, Actual: actual, Description: '1 from 2d2'});
    } catch(e){testResults.push({Error: e, Description: '1 from 2d2'});}
 
    try{
-   actual = probabilityThat_XofNIsA_GivenThatXofNisLargest(2, new Die(2), 2);
+   actual = beta.probabilityThat_XofNIsA_GivenThatXofNisLargest(2, new Die(2), 2);
    //all: 11, 12, 21, 22 so the x is: -, 2, 2, -
    testResults.push({Expected: 1, Actual: actual, Description: '2 in 2d2'});
    } catch(e){testResults.push({Error: e, Description: '2 in 2d2'});}
 
-   return TestRunner.displayResults('StackExchange probabilityThat_XofNIsA_GivenThatXofNisLargest', testResults, isFirst);
+   return TestRunner.displayResults('StackExchange beta.probabilityThat_XofNIsA_GivenThatXofNisLargest', testResults, isFirst);
 };
 TestSuite.StackExchange.probabilityThat_XofNisLargest_GivenThatXofNIsA = function(isFirst)
 {
@@ -124,23 +124,23 @@ TestSuite.StackExchange.probabilityThat_XofNisLargest_GivenThatXofNIsA = functio
    var testResults = [], actual;
 
    try{
-   actual = probabilityThat_XofNisLargest_GivenThatXofNIsA(1, new Die(2), 1);
+   actual = beta.probabilityThat_XofNisLargest_GivenThatXofNIsA(1, new Die(2), 1);
    testResults.push({Expected: 1, Actual: actual, Description: '1 in 1d2'});
    } catch(e){testResults.push({Error: e, Description: '1 in 1d2'});}
 
    try{
-   actual = probabilityThat_XofNisLargest_GivenThatXofNIsA(1, new Die(2), 2);
+   actual = beta.probabilityThat_XofNisLargest_GivenThatXofNIsA(1, new Die(2), 2);
    //because it can't be greater it can only be equal to
    testResults.push({Expected: 0, Actual: actual, Description: '1 in 2d2'});
    } catch(e){testResults.push({Error: e, Description: '1 in 2d2'});}
 
    try{
-   actual = probabilityThat_XofNisLargest_GivenThatXofNIsA(2, new Die(2), 2);
+   actual = beta.probabilityThat_XofNisLargest_GivenThatXofNIsA(2, new Die(2), 2);
    //all: 11, 12, 21, 22
    testResults.push({Expected: (2/4), Actual: actual, Description: '2 in 2d2'});
    } catch(e){testResults.push({Error: e, Description: '2 in 2d2'});}
 
-   return TestRunner.displayResults('StackExchange probabilityThat_XofNisLargest_GivenThatXofNIsA', testResults, isFirst);
+   return TestRunner.displayResults('StackExchange beta.probabilityThat_XofNisLargest_GivenThatXofNIsA', testResults, isFirst);
 };
 TestSuite.StackExchange.probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA = function(isFirst)
 {
@@ -149,32 +149,32 @@ TestSuite.StackExchange.probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA =
    var testResults = [], actual;
 
    try{
-   actual = probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(1, new Die(2), 1);
+   actual = beta.probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(1, new Die(2), 1);
    testResults.push({Expected: 1, Actual: actual, Description: '1d2'});
    } catch(e){testResults.push({Error: e, Description: '1d2'});}
 
    try{
-   actual = probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(1, new Die(2), 2);
+   actual = beta.probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(1, new Die(2), 2);
    testResults.push({Expected: 1, Actual: actual, Description: '2d2 given 1'});
    } catch(e){testResults.push({Error: e, Description: '2d2 given 1'});}
 
    try{
-   actual = probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(2, new Die(2), 2);
+   actual = beta.probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(2, new Die(2), 2);
    testResults.push({Expected: (1/2), Actual: actual, Description: '2d2 given 2'});
    } catch(e){testResults.push({Error: e, Description: '2d2 given 2'});}
 
    try{
-   actual = probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(2, new Die(3), 2);
+   actual = beta.probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(2, new Die(3), 2);
    testResults.push({Expected: (2/3), Actual: actual, Description: '2d3 given 2'});
    } catch(e){testResults.push({Error: e, Description: '2d3 given 2'});}
 
    try{
-   actual = probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(2, new Die(2), 3);
+   actual = beta.probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA(2, new Die(2), 3);
    //others: 11, 12, 21, 22
    testResults.push({Expected: (1/4), Actual: actual, Description: '3d2 given 2'});
    } catch(e){testResults.push({Error: e, Description: '3d2 given 2'});}
 
-   return TestRunner.displayResults('StackExchange probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA', testResults, isFirst);
+   return TestRunner.displayResults('StackExchange beta.probabilityThat_XofNisSmallestOrEqual_GivenThatXofNIsA', testResults, isFirst);
 };
 TestSuite.StackExchange.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual = function(isFirst)
 {
@@ -183,7 +183,7 @@ TestSuite.StackExchange.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqu
    var testResults = [], actual;
 
    try{
-   actual = probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(1, new Die(2), 1);
+   actual = beta.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(1, new Die(2), 1);
    //all: 11, 12, 21, 22
    //given (first die is n): 11, 12, -, 22
    //becomes (other die used by sum): 1, 2, 2
@@ -191,12 +191,12 @@ TestSuite.StackExchange.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqu
    } catch(e){testResults.push({Error: e, Description: '2d2 given 1'});}
 
    try{
-   actual = probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(2, new Die(2), 1);
+   actual = beta.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(2, new Die(2), 1);
    testResults.push({Expected: (2/3), Actual: actual, Description: '2d2 given 2'});
    } catch(e){testResults.push({Error: e, Description: '2d2 given 2'});}
 
    try{
-   actual = probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(3, new Die(2), 2);
+   actual = beta.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(3, new Die(2), 2);
    //all: 111, 112, 121, 122, 211, 212, 221, 222
    //given (first die is n): 111, 112, 121, 122, -, -, -, 222
    //becomes (other dice): 11, 12, 21, 22, 22
@@ -205,7 +205,7 @@ TestSuite.StackExchange.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqu
    } catch(e){testResults.push({Error: e, Description: '3d2 given 3'});}
 
    try{
-   actual = probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(2, new Die(3), 1);
+   actual = beta.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(2, new Die(3), 1);
    //all: 11, 12, 13, 21, 22, 23, 31, 32, 33
    //given (first die is n): 11, 12, 13, -, 22, 23, -, -, 33
    //becomes (other dice): 1, 2, 3, 2, 3, 3
@@ -214,12 +214,12 @@ TestSuite.StackExchange.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqu
    } catch(e){testResults.push({Error: e, Description: '2d3 given 2'});}
 
    try{
-   actual = probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(3, new Die(3), 1);
+   actual = beta.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(3, new Die(3), 1);
    testResults.push({Expected: (3/6), Actual: actual, Description: '2d3 given 3'});
    } catch(e){testResults.push({Error: e, Description: '2d3 given 3'});}
 
    try{
-   actual = probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(4, new Die(3), 2);
+   actual = beta.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual(4, new Die(3), 2);
    //all: 111, 112, 113, 121, 122, 123, 131, 132, 133, 211, 212, 213, 221, 222, 223, 231, 232, 233, 311, 312, 313, 321, 322, 323, 331, 332, 333
    //given (first die is n): 111, 112, 113, 121, 122, 123, 131, 132, 133, -, -, -, -, 222, 223, -, 232, 233, -, -, -, -, -, -, -, -, 333
    //becomes (other dice): 11, 12, 13, 21, 22, 23, 31, 32, 33, 22, 23, 32, 33, 33
@@ -228,5 +228,5 @@ TestSuite.StackExchange.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqu
    testResults.push({Expected: (4/14), Actual: actual, Description: '3d3 given 4'});
    } catch(e){testResults.push({Error: e, Description: '3d3 given 4'});}
 
-   return TestRunner.displayResults('StackExchange probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual', testResults, isFirst);
+   return TestRunner.displayResults('StackExchange beta.probabilityThatSumOfDiceIsA_GivenThatXofNisSmallestOrEqual', testResults, isFirst);
 };
