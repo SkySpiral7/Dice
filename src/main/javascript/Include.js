303afa78e9bc3845906fe1647afb44550de8089e
Ryan Worcester <rworcest@g.emporia.edu>
2017-04-27 20:23:23 -0500

    Redid include files

--- src/main/javascript/Include.js
+++ src/main/javascript/Include.js
@@ -1,19 +1,16 @@
 'use strict';
 
-//TODO: allow a way to say include "core", "statistics", "all"?
 var fileNames = ['core/DicePool', 'core/Die', 'core/Parser', 'core/Prototypes', 'core/Validation']
-.concat(['statistics/DiceExpression', 'statistics/Draw', 'statistics/GenerateHtml', 'statistics/Main', 'statistics/Misc'])
-.concat(['gameSpecific/L5R', 'gameSpecific/Mistborn', 'gameSpecific/Pathfinder', 'gameSpecific/Warhammer']);
+.concat(['statistics/DiceExpression', 'statistics/Draw', 'statistics/GenerateHtml', 'statistics/Main', 'statistics/Misc']);
+//don't include gameSpecific since the client likely only wants 1 or 0
 
-var includePath;  //this doesn't delete a pre-existing value.
-//I could loop over document.getElementsByTagName('script') but that's too much work and equal assumption
-if(null !== document.getElementById('DiceInclude')) includePath = document.getElementById('DiceInclude').src.replace('Include.js', '');
-else if(undefined === includePath) includePath = '';
+var includePath = document.getElementById('DiceInclude').src.replace('Include.js', '');
 
-for (var i = 0; i < fileNames.length; ++i)
+function addScripts()
 {
-   document.write('<script type="text/javaScript" src="' + includePath + fileNames[i] + '.js"></script>');
+   for (var i = 0; i < fileNames.length; ++i)
+   {
+      document.write('<script type="text/javaScript" src="' + includePath + fileNames[i] + '.js"></script>');
+   }
 }
-
-//change this here so that the test/javascript/include.js can be passed a specific includePath
-includePath = includePath.replace('main/', 'test/');
+addScripts();
--- src/test/javascript/Include.js
+++ src/test/javascript/Include.js
@@ -1,15 +1,20 @@
 'use strict';
 
+//since I'm testing everything I need to include every file
+fileNames = fileNames.concat(['gameSpecific/L5R', 'gameSpecific/Mistborn', 'gameSpecific/Pathfinder', 'gameSpecific/Warhammer']);
+addScripts();
+
+//addScripts, fileNames, includePath are defined in main/javascript/Include.js
+includePath = includePath.replace('main/', 'test/');
+
+//these have no tests
 fileNames.removeElement('core/Validation');
 fileNames.removeElement('statistics/Draw');
 fileNames.removeElement('statistics/Misc');
+
+//need this in order to test
 fileNames.push('Testing Util');
 
-//includePath was defined in main/javascript/include.js
+addScripts();
 
 TestConfig.defaultDelta = Number.EPSILON;  //since this program only deals with natural numbers and statistics
-
-for (var i = 0; i < fileNames.length; ++i)
-{
-   document.write('<script type="text/javaScript" src="' + includePath + fileNames[i] + '.js"></script>');
-}
