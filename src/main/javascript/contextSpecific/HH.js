5042ade3cddbe75b896182b3d8b6aaca552ad24a
Ryan Worcester <rworcest@g.emporia.edu>
2019-10-27 17:26:49 -0500

    HH.Damage: critical hit in output, object input

--- src/main/javascript/contextSpecific/HH.js
+++ src/main/javascript/contextSpecific/HH.js
@@ -19,42 +19,47 @@ HH.PowerCheckVsDc = function(rank, difficultyClass, randomSource)
 };
 //TODO: other attacks are possible
 //TODO: input: {attacker: {attack: 0, damageRank: 0, lethal: true}, defender: {hp: 5, condition: null, activeDefense: 0, toughness: 0}}, randomSource
-HH.Damage = function(attack, activeDefense, damageRank, toughness, randomSource)  //TODO: non-lethal is possible but requires HP
+HH.Damage = function(input, randomSource)
 {
-   var diceSum = 0, attackThreat = false, normalHit = true;
+   var diceSum = 0, criticalHit = false;
    var fudgePool = new DicePool('3dF');
-   if (undefined !== attack)  //Perception range
+   if (undefined !== input.attack)  //Perception range
    {
       diceSum = fudgePool.sumRoll(randomSource);
       if(-3 === diceSum) return {attack: 'Critical Miss', toString: function(){return 'Critical Miss';}};  //Critical Failure is an Automatic failure
-      attackThreat = (3 === diceSum);
-      var attackResult = diceSum + attack;
+      var attackThreat = (3 === diceSum);
+      var attackResult = diceSum + input.attack;
       var activeDefenseResult = 0;  //defenseless uses 0
       //TODO: also support vulnerable (half the result)
-      if (undefined !== activeDefense)
+      if (undefined !== input.activeDefense)
       {
          diceSum = fudgePool.sumRoll(randomSource);
          if(-3 === diceSum) activeDefenseResult = -Infinity;  //Critical Failure is an Automatic failure
          else if(3 === diceSum) activeDefenseResult = 4;  //Critical Success is +1
-         else activeDefenseResult = diceSum + activeDefense;
+         else activeDefenseResult = diceSum + input.activeDefense;
       }
-      normalHit = (attackResult >= activeDefenseResult);
+      var normalHit = (attackResult >= activeDefenseResult);
       if(!attackThreat && !normalHit) return {attack: 'Miss', toString: function(){return 'Miss';}};
+      criticalHit = (attackThreat && normalHit);  //critical hit confirmed
    }
    diceSum = fudgePool.sumRoll(randomSource);
    if(3 === diceSum) ++diceSum;
    else if(-3 === diceSum) --diceSum;
-   var damageRolled = diceSum + damageRank;
-   if(attackThreat && normalHit) ++damageRolled;  //critical hit confirmed. TODO: can also be used for Added Effect
+   var damageRolled = diceSum + input.damageRank;
+   var attackString = criticalHit ? 'Critical Hit' : 'Hit';
+   if(criticalHit) ++damageRolled;  //can also be used for Added Effect but uses Increased Effect by default.
+   // Caller will have to check attack and -1 if they don't want the +1
+   //TODO: what about crit vs minion?
+   //"Against a minion, this bypasses the defense check entirely; the minion automatically receives the highest degree of the attack's effect."
+   //what does that mean?
 
    diceSum = fudgePool.sumRoll(randomSource);
    if(3 === diceSum) ++diceSum;
    else if(-3 === diceSum) --diceSum;
-   var toughnessRolled = diceSum + toughness;
+   var toughnessRolled = diceSum + input.toughness;
 
    var damageDealt = damageRolled - toughnessRolled;
-   //TODO: include critical hit
-   if(damageDealt < 0) return {attack: 'Hit', toString: function(){return 'Damage failed';}};  //damage: undefined
+   if(damageDealt < 0) return {attack: attackString, toString: function(){return 'Damage failed';}};  //damage: undefined
 
-   return {attack: 'Hit', damage: damageDealt, toString: function(){return damageDealt + ' Damage';}};  //0 is ok
+   return {attack: attackString, damage: damageDealt, toString: function(){return damageDealt + ' Damage';}};  //0 is ok
 };
